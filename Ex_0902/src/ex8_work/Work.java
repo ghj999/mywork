0d7235f8321f03bb02be158c74ecc92212c59ec5
user1 <user1@DESKTOP-TIQMMPJ>
2024-09-03 17:42:27 +0900

    0903

---
+++ Ex_0902/src/ex8_work/Work.java
@@ -0,0 +1,22 @@
+package ex8_work;
+
+public class Work {
+
+	private String result = "";
+	
+	public void pritnOdd( String ) {
+	
+		for ( int i = 0; i < input.length(); i += 2 ) {
+			char ch = input.charAT(i);
+			System.out.print(ch);
+		}//for
+		
+		// for ( int i = 0; i < input.length(); i++ ) {
+			//if( i % 2 == 0 ) {
+			//	result += input.charAt(i);
+			//}
+		}//for
+		//System.out.println(result);
+		
+	}
+}
--- Ex_0902/src/ex8_work/WorkMain.java
+++ Ex_0902/src/ex8_work/WorkMain.java
@@ -1,5 +1,7 @@
 package ex8_work;
 
+import java.util.Scanner;
+
 public class WorkMain {
 	public static void main(String[] args) {
 		//키보드에서 입력받은 문장의 홀수번째 문자만 추출하여 결과로 보여주기
@@ -9,5 +11,15 @@ public class WorkMain {
 		
 		//if문 써서랑, 안 써서랑. 2가지로.
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("입력 : ");
+		String input = sc.next();
+		
+		Work wk = new Work();
+		wk.printOdd(input);
+		
 	}//main
 }
+
+
+//이건 난이도 높은 편 아냐. 
